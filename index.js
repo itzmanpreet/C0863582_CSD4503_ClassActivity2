20bde7152f96cb2d2bbe596a5e258ec5ac3a8712
itzmanpreet <c0863582@mylambton.ca>
2024-02-17 12:26:18 -0500

    new endpoint added

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const SERVER_PORT = process.env.PORT || 3000;
 app.use(express.static("../index.html"));
 
 // http://localhost:3000/
+app.get('/', function (req, res) {
+    res.send('<h1>Hello -Cloud Computing</h1> ')
+    })
+
+// http://localhost:3000/C0867393
 app.get('/C0867393', function (req, res) {
 res.send('<h1>Hello - C0863582 – Manpreet Singh</h1> ')
 })
